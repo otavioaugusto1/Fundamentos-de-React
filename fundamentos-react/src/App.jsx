50640c0f8d87ec561904d66700fa130e79f2f45c
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-12 15:17:35 -0300

    Modulo 1

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-
+import "./App.css"
 import Card from './components/layout/Card'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -9,14 +9,23 @@ import Aleatorio from './components/basicos/Aleatorio'
 
 // export default function App(props){}
 export default props =>
-    <div>
+    <div className ="App">
         <h1>Fundamentos React</h1>
-        <Card titulo = "Card"/>
-        <Aleatorio min = {1.0} max = {10}/>
-        <Fragmento />
-        <ComParametro titulo="Segundo componente"
-            subtitulo="muito legal"
-            nota={5.0} />
-        <Primeiro></Primeiro>
+        <div className="Cards">
+            <Card titulo="Desafio Aleatório" color = "#080">
+                <Aleatorio min={1.0} max={10} />
+            </Card>
+            <Card titulo="Fragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="Com parâmetro">
+                <ComParametro titulo="Segundo componente"
+                    subtitulo="muito legal"
+                    nota={5.0} />
+            </Card>
+            <Card titulo="Primeiro componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
 
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Card.css"
 export default (props) => {
+    const cardStyle = {
+        backgroundColor: props.color,
+        borderColor: props.color
+    }
     return(
-        <div >
-            <div>Conteúdo</div>
-            <div>{props.titulo}</div>
+        <div className = "Card" style= {cardStyle}>
+            <div className= "Title">{props.titulo}</div>
+            <div className = "Content">
+                {props.children}
+                </div>
         </div>
     )
 }
